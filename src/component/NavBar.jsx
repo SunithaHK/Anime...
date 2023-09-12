6c92ab8a2ebe51614b610040df953ca90521b75d
Sunitha_hk <sunithahk95@gmail.com>
2023-09-12 11:59:43 +0530

    Title

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -9,7 +9,7 @@ function Navbar(){
           <div className='logo'>
             <a href="/" ><img src={photo} alt="logo" /> </a>
           </div>
-          <div className='anime'><h1>Anime</h1></div>
+          <div className='anime'><h1>Anime....</h1></div>
           <div className='search'>
             <input type="search" value={v} onChange={(e)=>setv(e.target.value)}/>
             {/* target is method in value and this method will return input tag */}
